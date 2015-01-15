8567b8bb643ca7e556af4db3cf1b52e4e0b0a11d
Jared Dickson <code@jareddickson.com>
2015-01-15 15:54:11 -0500

    Add tests

---
+++ test/test-load.js
@@ -0,0 +1,34 @@
+'use strict';
+var assert = require('assert');
+
+describe('dragon-names loaded', function () {
+
+    beforeEach(function () {
+        this.dragonNames = require('../');
+    });
+
+    it('has animated dragon names', function () {
+        assert(this.dragonNames.animated.length > 0);
+    });
+
+    it('has comic dragon names', function () {
+        assert(this.dragonNames.comic.length > 0);
+    });
+
+    it('has film dragon names', function () {
+        assert(this.dragonNames.film.length > 0);
+    });
+
+    it('has game dragon names', function () {
+        assert(this.dragonNames.game.length > 0);
+    });
+
+    it('has literatur dragon names', function () {
+        assert(this.dragonNames.literature.length > 0);
+    });
+
+    it('has television dragon names', function () {
+        assert(this.dragonNames.television.length > 0);
+    });
+});
+
