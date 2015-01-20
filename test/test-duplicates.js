0f05cb696fa498d9563642381b7fb92ad088e6bd
Jared Dickson <code@jareddickson.com>
2015-01-19 19:45:43 -0500

    Report duplicate names on all dragons test

--- test/test-duplicates.js
+++ test/test-duplicates.js
@@ -9,31 +9,31 @@ describe('dragon-names has no duplicates', function () {
     });
 
     it('animated dragon names has no duplicates', function () {
-        assert(this.dragonNames.animated.length === _.unique(this.dragonNames.animated).length);
+        assert(this.dragonNames.animated.length === _.uniq(this.dragonNames.animated).length);
     });
 
     it('comic dragon names has no duplicates', function () {
-        assert(this.dragonNames.comic.length === _.unique(this.dragonNames.comic).length);
+        assert(this.dragonNames.comic.length === _.uniq(this.dragonNames.comic).length);
     });
 
     it('film dragon names has no duplicates', function () {
-        assert(this.dragonNames.film.length === _.unique(this.dragonNames.film).length);
+        assert(this.dragonNames.film.length === _.uniq(this.dragonNames.film).length);
     });
 
     it('game dragon names has no duplicates', function () {
-        assert(this.dragonNames.game.length === _.unique(this.dragonNames.game).length);
+        assert(this.dragonNames.game.length === _.uniq(this.dragonNames.game).length);
     });
 
     it('literatur dragon names has no duplicates', function () {
-        assert(this.dragonNames.literature.length === _.unique(this.dragonNames.literature).length);
+        assert(this.dragonNames.literature.length === _.uniq(this.dragonNames.literature).length);
     });
 
     it('television dragon names has no duplicates', function () {
-        assert(this.dragonNames.television.length === _.unique(this.dragonNames.television).length);
+        assert(this.dragonNames.television.length === _.uniq(this.dragonNames.television).length);
     });
 
-    it('all dragon names has no duplicates', function() {
-        assert(this.dragonNames.all.length === _.unique(this.dragonNames.all).length);
+    it('all dragon names has no duplicates ', function() {
+        assert(this.dragonNames.all.length === _.uniq(this.dragonNames.all).length, _.chain(dragonNames.all).groupBy().filter(function(item) { return item.length > 1; }).flatten().uniq().value());
     });
 });
 
